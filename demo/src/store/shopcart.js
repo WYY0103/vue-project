45b30887ec2f3d5ddc82dc6b946777a178cc5e91
WYY0103 <3177620483@qq.com>
2022-08-28 16:31:57 +0800

    feat：动态展示购物车数据

--- demo/src/store/shopcart.js
+++ demo/src/store/shopcart.js
@@ -1,22 +1,31 @@
-import {reqGetShoppingCart} from '@/api'
+import { reqGetShoppingCart } from '@/api'
 
 const state = {
+    cartList: [],
 
 };
+
 const mutations = {
-    
-   
+    GETCARTLIST(state, cartList) {
+        state.cartList = cartList;
+    }
 };
+
 const actions = {
-    async getCartList({commit}){
+    async getCartList({ commit }) {
         let res = await reqGetShoppingCart();
         console.log(res);
+        if (res.code == 200) {
+            commit('GETCARTLIST', res.data);
+        }
     }
-   
+
 };
 
 const getters = {
-   
+    cartList(state){
+        return state.cartList[0] || {};
+    }
 };
 
 export default {
