4ab8b86e930f9ae629a35b82f4f21fba734bf4f5
WYY0103 <3177620483@qq.com>
2022-09-01 16:07:24 +0800

    feat：路由守卫登录操作

--- demo/src/router/index.js
+++ demo/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import route from './route'
+import route from './route';
+//引入仓库
+import store from '@/store';
 
 // 使用router插件
 Vue.use(VueRouter);
@@ -33,10 +35,60 @@ VueRouter.prototype.replace = function (location, resolve, reject) {
 
 
 // 配置路由
-export default new VueRouter({
+const router = new VueRouter({
     routes: route,
     // return 期望滚动到哪个的位置  滚动条的位置
     scrollBehavior() {
         return { y: 0 }
     }
 })
+
+export default router;
+
+
+//全局守卫:只要项目中有任何路由变化，全局守卫都会进行拦截【符合条件走你，不符合条件不能访问】
+//全局守卫:全局前置守卫【访问之前进行触发】
+//全局前置守卫
+router.beforeEach(async (to, from, next) => {
+    //to:去的那个路由的信息
+    //from:从那个路由而来的信息
+    //next:放行函数
+    //第一种：next(),放行函数，放行到它想去的路由
+    //第二种:next(path),守卫指定放行到那个路由去
+
+    //用户是否登录:取决于仓库里面是否有token
+    //每一次路由跳转之前需要用有用户信息在跳转,没有发请求获取用户信息在跳转
+    //token
+    let hasToken = store.state.user.token;
+    //用户信息
+    let hasNickName = store.state.user.nickName;
+    //用户登录
+    if (hasToken) {
+        //用户登录了,不能去login
+        if (to.path == "/login") {
+            next('/home');
+        } else {
+            //用户登陆了,而且还有用户信息【去的并非是login】
+            if (hasNickName) {
+                next();
+            } else {
+                //用户登陆了,但是没有用户信息 
+                try {
+                    //发请求获取用户信息以后在放行
+                    await store.dispatch('getUserInfo');
+                    next();
+                } catch (error) {
+                    //用户没有信息，还携带token发请求获取用户信息【失败】
+                    //token失效:本地清空数据、服务器的token通知服务器清除
+                    await store.dispatch('logout');
+                    //回到登录页，重新获取一个新的token
+                    next('/login');
+                }
+            }
+        }
+    } else {
+        //用户未登录||目前的判断都是放行.将来这里会'回手掏'增加一些判断
+        //用户未登录:不能进入/trade、/pay、/paysuccess、/center、/center/myorder  /center/teamorder
+       next();
+    }
+});
